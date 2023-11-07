aa8b1215169a4c109d53ea3dfb22656212268043
agungsapp <agung.dni19@gmail.com>
2023-11-07 13:19:24 +0700

    create midleware and a function to handle posible error

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body);
 
   const { username, email, password } = req.body; //parsing data json yang dikirim di ambil key nya dari body request.
@@ -15,14 +16,12 @@ export const signup = async (req, res) => {
   // insert to database mongo
   const newUser = new User({ username, email, password: hashPassword });
 
-
   try {
     await newUser.save();
     res.status(201).json("User created successfully")
   } catch (error) {
-    res.status(500).json(error.message);
+    // res.status(500).json(error.message);
+    next(errorHandler(550, 'error from the function'));
   }
-
-
 
 }
